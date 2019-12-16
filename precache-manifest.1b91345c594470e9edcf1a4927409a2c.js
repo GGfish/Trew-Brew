fcd231fd7d2d6ae34383107808b812d645bb5cf2
Bradygg <gordishbg@gmail.com>
2019-12-16 09:45:38 -0500

    Updates

---
+++ precache-manifest.1b91345c594470e9edcf1a4927409a2c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ec8b7381e83fe3b4b61e0fd2ff77b56c",
+    "url": "/trew-brew/index.html"
+  },
+  {
+    "revision": "15334cfe2a4e3ddb48e0",
+    "url": "/trew-brew/static/css/main.8322c587.chunk.css"
+  },
+  {
+    "revision": "044d7d7dd281a8d0c1e1",
+    "url": "/trew-brew/static/js/2.617652a7.chunk.js"
+  },
+  {
+    "revision": "e928fe768baa9832b5bc57eae021f30c",
+    "url": "/trew-brew/static/js/2.617652a7.chunk.js.LICENSE"
+  },
+  {
+    "revision": "15334cfe2a4e3ddb48e0",
+    "url": "/trew-brew/static/js/main.a817dece.chunk.js"
+  },
+  {
+    "revision": "cf8b2c598ced4eac53de",
+    "url": "/trew-brew/static/js/runtime-main.f8fafe65.js"
+  }
+]);
